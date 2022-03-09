125bdd9b006acd478dac2d12951bda9809ad3a53
Lerqiu <mcwk810@gmail.com>
2022-03-09 23:31:21 +0100

    Finished

---
+++ src/Components/HelloWorld.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './HelloWorld.css';
+
+function HelloWorld() {
+    return (
+        <div>
+            <p className="PHEllo">
+                Hello
+            </p>
+            <p className="PHEllo">
+                World
+            </p >
+        </div >
+    );
+}
+
+export default HelloWorld;
